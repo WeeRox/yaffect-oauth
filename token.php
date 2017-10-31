774b1118c839b03c704b648fd82248562fd5a95a
Aron Bergman <bathingrad@gmail.com>
2017-10-31 19:16:08 +0100

    Created autoloader

--- token.php
+++ token.php
@@ -1,4 +1,14 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
+
+// Register an autoloader for classes.
+// The namespace will correspond to folder structure
+spl_autoload_register(function ($class)
+{
+  $file = str_replace('\\', '/', $class) . '.php';
+  if (file_exists($file)) {
+    include $file;
+  }
+});
 ?>
