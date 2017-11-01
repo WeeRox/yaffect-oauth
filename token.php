cefc63331532e655da6ad098afcd6f99d749971a
Aron Bergman <bathingrad@gmail.com>
2017-11-01 18:36:14 +0100

    Created a check for grant type with error response

--- token.php
+++ token.php
@@ -2,6 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
+use Response\ErrorResponse;
+
 // Register an autoloader for classes.
 // The namespace will correspond to folder structure
 spl_autoload_register(function ($class)
@@ -17,6 +19,15 @@ $config = include "config.php";
 
 //Create a connection to the MySQL server
 $db = new mysqli($config['hostname'], $config['username'], $config['password'], $config['database']);
+
+switch ($_POST['grant_type'] {
+  case 'password':
+    //TODO
+    break;
+  default:
+    ErrorResponse::unsupportedGrantType($_POST['grant_type']);
+    break;
+}
 
 $db->close();
 ?>
