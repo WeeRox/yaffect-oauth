c4547829d45e57460bd4bfeade86ccaea00697ad
Aron Bergman <bathingrad@gmail.com>
2017-11-01 16:45:53 +0100

    Created class for handling error reponses

---
+++ Response/ErrorResponse.php
@@ -0,0 +1,48 @@
+<?php
+namespace Reponse;
+
+class ErrorResponse
+{
+  private $response = array();
+  function __construct(argument)
+  {
+    http_response_code(400);
+    header("Cache-Control: no-store");
+    header("Pragma: no-cache");
+    header("Content-Type: application/json");
+  }
+
+  static function invalidRequest()
+  {
+
+  }
+
+  static function invalidClient()
+  {
+
+  }
+
+  static function invalidGrant()
+  {
+
+  }
+
+  static function unauthorizedClient()
+  {
+
+  }
+
+  static function unsupportedGrantType($grantType)
+  {
+    $response['error'] = 'unsupported_grant_type';
+    $response['error_description'] = "Grant type '$grantType' is unsupported. Supported grant types are 'password'.";
+    echo $response;
+  }
+
+  static function invalidScope()
+  {
+
+  }
+}
+
+?>
