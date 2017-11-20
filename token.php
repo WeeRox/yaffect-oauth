f3ec4707489261780eea6d852df9ae5301829c86
Aron Bergman <bathingrad@gmail.com>
2017-11-20 20:32:27 +0100

    Removed the database connection code

--- token.php
+++ token.php
@@ -3,6 +3,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
 use Response\ErrorResponse;
+use Grant\ResourceOwnerPasswordCredentialsGrant;
+use Database\Database;
 
 // Register an autoloader for classes.
 // The namespace will correspond to folder structure
@@ -14,24 +16,16 @@ spl_autoload_register(function ($class)
   }
 });
 
-//Include config for MySQL server
-$config = include "config.php";
-
-//Create a connection to the MySQL server
-$db = new mysqli($config['hostname'], $config['username'], $config['password'], $config['database']);
-
 if (empty($_POST['grant_type'])) {
   ErrorResponse::invalidRequest();
 } else {
   switch ($_POST['grant_type']) {
     case 'password':
-    //TODO
-    break;
+      ResourceOwnerPasswordCredentialsGrant::respond();
+      break;
     default:
-    ErrorResponse::unsupportedGrantType($_POST['grant_type']);
-    break;
+      ErrorResponse::unsupportedGrantType($_POST['grant_type']);
+      break;
   }
 }
-
-$db->close();
 ?>
