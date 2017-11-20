290785af63184bc1dd5057cfdc980783925b40a4
Aron Bergman <bathingrad@gmail.com>
2017-11-20 20:32:56 +0100

    Created methods for granting access
    Created the three following methods to make sure that the token request is okay before issuing an access token:
    validateRequest(): checks the request for required parameters
    validateClient(): checks the authentication and makes sure it is done correctly
    validateUser(): makes sure that the user info matches a user in the database

--- Grant/ResourceOwnerPasswordCredentialsGrant.php
+++ Grant/ResourceOwnerPasswordCredentialsGrant.php
@@ -1,3 +1,54 @@
 <?php
+namespace Grant;
 
+use Response\ErrorResponse;
+
+class ResourceOwnerPasswordCredentialsGrant
+{
+
+  public $client;
+
+  public static function respond()
+  {
+    if (self::validateRequest() && self::validateClient() && self::validateUser()) {
+      //TODO: return access token (bearer response)
+    }
+  }
+
+  private static function validateRequest()
+  {
+    // TODO: check that the client didn't send multiple credentials
+
+    // Check that all required parameters is present
+    if (empty($_POST['username']) || empty($_POST['password'])) {
+      ErrorResponse::invalidRequest();
+      return false;
+    }
+    return true;
+  }
+
+  private static function validateClient()
+  {
+    if (explode(" ", $_SERVER['HTTP_AUTHORIZATION'])[0] == 'Basic') {
+      if (!empty($_SERVER['PHP_AUTH_USER'])) {
+        // TODO: get client info from database
+        // TODO: if client id weren't found, return error invalid_client
+        // TODO: check whether the client were issued a client secret and if so check that it matches the one in the request
+      } else {
+        // no id were passed in the authorization header
+        ErrorResponse::invalidClien();
+        return false;
+      }
+    } else {
+      // the authentication type weren't Basic
+      ErrorResponse::invalidClient();
+      return false;
+    }
+  }
+
+  private static function validateUser()
+  {
+    
+  }
+}
 ?>
