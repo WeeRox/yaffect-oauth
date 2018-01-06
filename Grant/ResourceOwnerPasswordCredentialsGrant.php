a69d00f5e2bcec76e33c4aa5c287c3be4b9b6bb4
Aron Bergman <bathingrad@gmail.com>
2018-01-06 18:46:27 +0100

    Added check for multiple credentials and unsupported auth methods

--- Grant/ResourceOwnerPasswordCredentialsGrant.php
+++ Grant/ResourceOwnerPasswordCredentialsGrant.php
@@ -17,9 +17,19 @@ class ResourceOwnerPasswordCredentialsGrant
 
   private static function validateRequest()
   {
-    // TODO: check that the client didn't send multiple credentials
+    // check that the client sent some auth info in either the header or the body
+    if (!empty($_SERVER['HTTP_AUTHORIZATION']) || !empty($_POST['client_id'])) {
+      // check that the client didn't send multiple credentials
+      if (!empty($_SERVER['HTTP_AUTHORIZATION']) && !empty($_POST['client_id'])) {
+        ErrorResponse::invalidRequest();
+        return false;
+      }
+    } else {
+      ErrorResponse::invalidRequest();
+      return false;
+    }
 
-    // Check that all required parameters is present
+    // check that all required parameters is present
     if (empty($_POST['username']) || empty($_POST['password'])) {
       ErrorResponse::invalidRequest();
       return false;
@@ -29,21 +39,33 @@ class ResourceOwnerPasswordCredentialsGrant
 
   private static function validateClient()
   {
-    if (explode(" ", $_SERVER['HTTP_AUTHORIZATION'])[0] == 'Basic') {
-      if (!empty($_SERVER['PHP_AUTH_USER'])) {
-        // TODO: get client info from database
-        // TODO: if client id weren't found, return error invalid_client
-        // TODO: check whether the client were issued a client secret and if so check that it matches the one in the request
-      } else {
-        // no id were passed in the authorization header
-        ErrorResponse::invalidClient();
-        return false;
-      }
-    } else {
-      // the authentication type weren't Basic
+    echo "validateClient<br />";
+
+    // check for unsupported auth methods
+    if (!empty($_SERVER['HTTP_AUTHORIZATION']) && (explode(" ", $_SERVER['HTTP_AUTHORIZATION'])[0] != 'Basic')) {
       ErrorResponse::invalidClient();
       return false;
     }
+
+    // check if client want to authenticate using header or body
+    if (!empty($_SERVER['HTTP_AUTHORIZATION'])) {
+      $clientId = $_SERVER['PHP_AUTH_USER'];
+      $clientSecret = $_SERVER['PHP_AUTH_PW'];
+    } else if (!empty($_POST['client_id'])) {
+      $clientId = $_POST['client_id'];
+      if (empty($_POST['client_secret'])) {
+        $clientSecret = NULL;
+      } else {
+        $clientSecret = $_POST['client_secret'];
+      }
+    }
+
+    echo "clientId: " . $clientId . "<br />";
+    echo "clientSecret: '" . $clientSecret . "'<br />";
+
+    // TODO: get client info from database
+    // TODO: if client id weren't found, return error invalid_client
+    // TODO: check whether the client were issued a client secret and if so check that it matches the one in the request
   }
 
   private static function validateUser()
